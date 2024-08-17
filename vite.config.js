ab10267a6ea30ae43633a59646fe3d5f665f0cd4
sudhanshu <sudhanshusoni45@gmail.com>
2024-08-17 20:47:05 +0530

    feat: basic folder structure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    sourcemap: true, // Enable source maps for debugging.
+    minify: "esbuild", // Use esbuild for minification.
+  },
+  css: {
+    modules: {
+      generateScopedName: "[name]__[local]", // This configuration disables obfuscation
+    },
+  },
+});
